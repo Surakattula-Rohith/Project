51728ac6b9331ace908cc26986204d69be70caaa
srohith.charan <srohith.charan@gmail.com>
2024-05-29 11:11:58 +0530

    Changed the snapshot version to enable the eureka client and server dependencies to be resolved.

--- eureka-server/src/main/java/com/stayplanner/eureka_server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/stayplanner/eureka_server/EurekaServerApplication.java
@@ -1,16 +1,16 @@
-package com.stayplanner.hotel_service;
+package com.stayplanner.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class HotelServiceApplication {
+@EnableEurekaServer
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HotelServiceApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
 }
