ce2df976d2a063875ab9c3f44bf6fe821858ec50
srohith.charan <srohith.charan@gmail.com>
2024-06-20 12:22:52 +0530

    Completed the add hotel owner endpoint and tested.

--- webapp/src/app/components/hotelowner/hotelowner-addhotel/hotelowner-addhotel.component.ts
+++ webapp/src/app/components/hotelowner/hotelowner-addhotel/hotelowner-addhotel.component.ts
@@ -4,23 +4,69 @@ import { FormsModule, NgForm } from '@angular/forms';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
+import { HotelService } from '../../../services/hotelowner/hotelowner.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-hotelowner-addhotel',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  providers: [HotelService],
+  imports: [CommonModule, FormsModule,HttpClientModule],
   templateUrl: './hotelowner-addhotel.component.html',
   styleUrl: './hotelowner-addhotel.component.css'
 })
-export class HotelownerAddhotelComponent {
+export class HotelownerAddhotelComponent { 
+  
+  ngOnInit(): void {}
 
-  hotelOwner: HotelOwner = {}; 
+  constructor(private hotelService : HotelService){
+    this.hotelOwner = new HotelOwner() ;
+  }
 
-  onSubmit(form: NgForm) {
-    if (form.valid) {
-      console.log('Form submitted!', this.hotelOwner);
-    } else {
-      console.log('Form is invalid!');
-    }
+  hotelOwner: HotelOwner = {
+    hotelName: '',
+    state: '',
+    city: '',
+    address: '',
+    gstNumber: '',
+    ownerName: '',
+    description: '',
+    ac: false,
+    wifi: false,
+    maintainance: false,
+    singleRooms: 0,
+    doubleRooms: 0,
+    tripleRooms: 0,
+    adminAccess: false
+  };
+
+  onSubmit(myForm: NgForm) {
+    
+    this.hotelOwner = {
+      hotelName: myForm.value.hotelName,
+      state: myForm.value.state,
+      city: myForm.value.city,
+      address: myForm.value.address,
+      gstNumber: myForm.value.gstNumber,
+      ownerName: myForm.value.ownerName,
+      description: myForm.value.description,
+      ac: myForm.value.ac || false,
+      wifi: myForm.value.wifi || false,
+      maintainance: myForm.value.maintainance || false,
+      singleRooms: myForm.value.singleRooms || 0,
+      doubleRooms: myForm.value.doubleRooms || 0,
+      tripleRooms: myForm.value.tripleRooms || 0,
+      adminAccess: myForm.value.adminAccess || false
+    };
+
+    this.hotelService.addHotel(this.hotelOwner).subscribe(
+      (response: any) => {
+        this.hotelOwner = new HotelOwner() ;
+        alert('Owner Added Successfully');
+      },
+      (error) => {
+        alert('Add valid details');
+      }
+    );
   }
 }
